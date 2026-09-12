67827fdbe83c7202fbdbb6bdde0367f98d00b1cd
0AllEX04 <cardenasdyago@gmail.com>
2026-09-12 18:57:40 -0500

    test: conexion SQL

--- app/src/main/java/com/example/todobarato/MainActivity.kt
+++ app/src/main/java/com/example/todobarato/MainActivity.kt
@@ -5,12 +5,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-
+import android.util.Log
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        val dbHelper = DatabaseHelper(this)
+        val db = dbHelper.readableDatabase
+
+        Log.d("TodoBarato", "Base de datos abierta correctamente: ${db.isOpen}")
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
