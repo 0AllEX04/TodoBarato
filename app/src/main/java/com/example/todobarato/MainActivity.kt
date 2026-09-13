b1be9e33633bab4d4e6e3e83a52a3e17abfd0a31
0AllEX04 <cardenasdyago@gmail.com>
2026-09-12 19:07:26 -0500

    feat: rawQuery para leer ventas

--- app/src/main/java/com/example/todobarato/MainActivity.kt
+++ app/src/main/java/com/example/todobarato/MainActivity.kt
@@ -14,7 +14,26 @@ class MainActivity : AppCompatActivity() {
         val dbHelper = DatabaseHelper(this)
         val db = dbHelper.readableDatabase
 
-        Log.d("TodoBarato", "Base de datos abierta correctamente: ${db.isOpen}")
+        Log.d("TODO_BARATO", "Base de datos abierta correctamente: ${db.isOpen}")
+
+        val cursor = db.rawQuery("SELECT * FROM ventas", null)
+
+        while (cursor.moveToNext()) {
+
+            val codigo = cursor.getString(cursor.getColumnIndexOrThrow("codigo"))
+            val nombre = cursor.getString(cursor.getColumnIndexOrThrow("nombre"))
+            val precio = cursor.getDouble(cursor.getColumnIndexOrThrow("precio"))
+            val cantidad = cursor.getInt(cursor.getColumnIndexOrThrow("cantidad"))
+            val tipo = cursor.getString(cursor.getColumnIndexOrThrow("tipo"))
+            val fechaVenta = cursor.getString(cursor.getColumnIndexOrThrow("fecha_venta"))
+
+            Log.d(
+                "TODO_BARATO",
+                "$codigo | $nombre | S/$precio | Cantidad: $cantidad | $tipo | $fechaVenta"
+            )
+        }
+
+        cursor.close()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
